572b7da9a6208a229f89da7af64d3f715f358471
themegamankingdom <aaron.roy99@yahoo.com>
2017-10-14 15:35:04 -0400

    Made TictacToe.java

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+/**
+ * Main.java	   Oct 14, 2017, 2:32:39 PM
+ */
+
+public class Main {
+
+  /**
+   * 
+   * 
+   * @param args
+   */
+  public static void main(String[] args) {
+    
+  }
+
+}
---
+++ src/TicTacToe.java
@@ -0,0 +1,53 @@
+
+
+public class TicTacToe{
+  //Create an array of all the positons of the board
+  int[] board = new int[9];
+  
+  public void add(int num, int pos){
+    if(board[pos] == 0){
+      board[pos] = num;
+    }
+    else{
+      System.out.println("Space already full at " + pos);
+    }
+  }
+  
+  public int hasWon(){
+    int[] results =  {isVertical(), isHorizontal(), isDiagonal()}; 
+    
+    for(int i = 0; i < results.length; i++){
+      if(results[i] != 0){
+        return results[i];
+      }
+    }
+    
+    return 0;
+  }
+       
+  private int isVertical(){
+    for(int i = 0; i <= 2;i++) {
+      if(board[0] != 0 && board[i] == board[i+3] && board[i+6] == board[i]) {
+        return board[i];
+      }
+    }
+    return 0;
+  }
+       
+  private int isHorizontal(){
+    for(int i = 0; i<3; i++){
+      if(board[0] != 0 && board[i] == board[i+1] && board[i] == board[i+2]){
+        return board[i];
+      }
+    }
+    return 0;
+  }
+   
+  private int isDiagonal(){
+    if ((board[0] != 0 && board [0] == board[4] && board[4] == board[8]) || 
+        (board[0] != 0 && board [2] == board[4] && board[4] == board[6]))
+      return board[4];
+    
+    return 0;
+  }
+}
